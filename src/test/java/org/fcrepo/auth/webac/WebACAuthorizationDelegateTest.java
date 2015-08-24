e1f3ce77ba6609eed35eb5c29b8c85ea97e01f03
Peter Eichman <peichman@umd.edu>
2015-08-24 16:00:06 -0400

    Added stub WebACAuthorizationDelegateTest JUnit test class.
    Stub contains one test method that fails with a "Not yet implemented" message.

---
+++ src/test/java/org/fcrepo/auth/webac/WebACAuthorizationDelegateTest.java
@@ -0,0 +1,14 @@
+package org.fcrepo.auth.webac;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class WebACAuthorizationDelegateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
