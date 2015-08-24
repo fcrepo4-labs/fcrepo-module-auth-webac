2cd2a296471b0a6352ec4c30897672e569bca034
Peter Eichman <peichman@umd.edu>
2015-08-24 16:17:41 -0400

    Expanded stub Authorization Delegate and unit test classes.
    Stub authz delegate logs calls to rolesHavePermission. The unit test mocks a session and tests the stub rolesHavePermission method that is currently always returning false. Code largely borrowed from the XACML authorization delegate.

--- src/main/java/org/fcrepo/auth/webac/WebACAuthorizationDelegate.java
+++ src/main/java/org/fcrepo/auth/webac/WebACAuthorizationDelegate.java
@@ -5,13 +5,27 @@ import java.util.Set;
 import javax.jcr.Session;
 
 import org.fcrepo.auth.roles.common.AbstractRolesAuthorizationDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebACAuthorizationDelegate extends AbstractRolesAuthorizationDelegate {
 
+    /**
+     * Class-level logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebACAuthorizationDelegate.class);
+    
 	@Override
 	public boolean rolesHavePermission(Session userSession, String absPath,
 			String[] actions, Set<String> roles) {
-		return false;
+		boolean permit = false;
+        LOGGER.debug("Request for actions: {}, on path: {}, with roles: {}. Permission={}",
+                actions,
+                absPath,
+                roles,
+                permit);
+        
+		return permit;
 	}
 
 }
--- src/test/java/org/fcrepo/auth/webac/WebACAuthorizationDelegateTest.java
+++ src/test/java/org/fcrepo/auth/webac/WebACAuthorizationDelegateTest.java
@@ -1,14 +1,43 @@
 package org.fcrepo.auth.webac;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.modeshape.jcr.api.Session;
 
 public class WebACAuthorizationDelegateTest {
 
+	private WebACAuthorizationDelegate webacAD;
+	
+    @Mock
+    private Session mockSession;
+	
+	@Before
+	public void setUp() {
+		webacAD = new WebACAuthorizationDelegate();
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertFalse(webacAD.rolesHavePermission(mockSession, "/fake/path", getFakeActions(), getFakeRoles()));
 	}
 
+    private static String[] getFakeActions() {
+        final String[] fakeActions =  new String[2];
+        fakeActions[0] = "fakeAction1";
+        fakeActions[1] = "fakeAction2";
+        return fakeActions;
+    }
+
+    private static Set<String> getFakeRoles() {
+        final Set<String> fakeRoles = new HashSet<>();
+        fakeRoles.add("fakeRole1");
+        fakeRoles.add("fakeRole2");
+        return fakeRoles;
+    }
 }
